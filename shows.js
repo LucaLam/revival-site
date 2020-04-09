7180a9efc8009c40e6b153f4f1d512a33f62534e
Luca Lamanna <luca.lamanna@hotmail.com>
2020-04-09 11:59:33 -0400

    shows automatically generated mapping data from an array

--- shows.js
+++ shows.js
@@ -1,27 +1,62 @@
-//creates row and adds class to row
-var div = document.createElement("div");
-div.classList.add("shows-table__row")
+var upComingShows = [{
+    date: "Sunday July 13, 2020",
+    artist: "The New Band",
+    charge: "$15"
+},
+{
+    date: "Friday July 22, 2020",
+    artist: "The Old Band",
+    charge: "$10"
+},
 
-//creates element and adds class
-var dateNode = document.createElement("div");
-dateNode.classList.add("shows-table__date")
+{
+    date: "Saturday July 23, 2020",
+    artist: "The Band",
+    charge: "$15"
+},
 
-var artistNode = document.createElement("div");
-artistNode.classList.add("shows-table__artist")
+{
+    date: "Sunday July 24, 2020",
+    artist: "Comedy Lunch",
+    charge: "$20"
+}
+]
 
-var chargeNode = document.createElement("div");
-chargeNode.classList.add("shows-table__charge")
+upComingShows.map(show => {
 
-//adds new node to row container
-div.appendChild(dateNode);
-div.appendChild(artistNode);
-div.appendChild(chargeNode);
+    var div, element, dateNode, artistNode, chargeNode;
 
-//inserts text into new element
-dateNode.innerText = "Sunday July 13, 2020"
-artistNode.innerText = "The New Band"
-chargeNode.innerText = "$15"
+    if(show){
+        
+        //creates row
+        div = document.createElement("div");
+        div.classList.add("shows-table__row")
 
-//attaches row to overall container
-var element = document.querySelector(".shows-table__container");
-element.appendChild(div);
+        //adds row to overall container
+        element = document.querySelector(".shows-table__container");
+        element.appendChild(div);
+
+        //creates elements for each category 
+        dateNode = document.createElement("div");
+        dateNode.classList.add("shows-table__date")
+
+        artistNode = document.createElement("div");
+        artistNode.classList.add("shows-table__artist")
+
+        chargeNode = document.createElement("div");
+        chargeNode.classList.add("shows-table__charge")
+
+        //adds new node to row container
+        div.appendChild(dateNode);
+        div.appendChild(artistNode);
+        div.appendChild(chargeNode);
+
+        //inserts text into new element
+        dateNode.innerText = show.date;
+        artistNode.innerText = show.artist
+        chargeNode.innerText = show.charge
+
+    }
+});
+
+
